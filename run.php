b918fba2681b8cce389afe1bf3dd6a534e538afd
Cal Henderson <cal@iamcal.com>
2013-06-10 17:28:56 -0700

    test collapsing

--- run.php
+++ run.php
@@ -7,7 +7,7 @@
 
 if (0){
 	$s = microtime(true);
-	$tokens = lex_sql($sql);
+	$tokens = $obj->lex($sql);
 	$e = microtime(true);
 
 	print_r($tokens);
@@ -16,6 +16,29 @@ if (0){
 	echo "Lexing took $ms ms\n";
 }
 
+if (1){
+	$s = microtime(true);
+	$tokens = $obj->lex($sql);
+	$e = microtime(true);
+	$tokens = $obj->collapse_tokens($tokens);
+	$e2 = microtime(true);
+
+	print_r($tokens);
+
+	$ms1 = round(1000 * ($e - $s));
+	$ms2 = round(1000 * ($e2 - $e));
+	echo "Lexing took $ms1 ms\n";
+	echo "Collapsing took $ms2 ms\n";
+}
+
+if (0){
+
+	$s = microtime(true);
 	$obj->parse($sql);
+	$e = microtime(true);
 
 	var_export($obj->tables);
+
+	$ms = round(1000 * ($e - $s));
+	echo "Parse took $ms ms\n";
+}
