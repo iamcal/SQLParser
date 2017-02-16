0902af528efe28245c9278a120fac2f9d99d0f32
Michael Demmer <mdemmer@slack-corp.com>
2017-02-16 12:06:34 -0800

    rework the lexer to keep track of source locations for each token
    In order to have the original CREATE TABLE text from the schema
    available when using the parsed structure, rework the lexer so that
    instead of extracting the substring tokens, it extracts tuples of
    [position, length] and maintains these associations when collapsing
    the token sequences.
    
    This means we can include the original source text in a `sql` field
    of the resulting parsed structure.

--- tests/10_full.php
+++ tests/10_full.php
@@ -10,6 +10,7 @@
 		$lines = array();
 		foreach ($obj->tables as $table){
 			$lines[] = "TABLE:{$table['name']}";
+			$lines[] = "SQL:{$table['sql']}";
 			foreach ($table['fields'] as $field){
 				$lines[] = "-FIELD:{$field['name']}:{$field['type']}";
 			}
@@ -22,7 +23,14 @@
 	plan(1);
 
 
-	full_test("CREATE TABLE table_name (a INT);", array(
-		"TABLE:table_name",
-		"-FIELD:a:INT",
+	full_test("CREATE TABLE table_name (a INT);\n" .
+		  "-- ignored comment\n\n" .
+		  "CREATE TABLE t2 (b VARCHAR)\n\n;\n",
+		array(
+			"TABLE:table_name",
+			"SQL:CREATE TABLE table_name (a INT);",
+			"-FIELD:a:INT",
+			"TABLE:t2",
+			"SQL:CREATE TABLE t2 (b VARCHAR)\n\n;",
+			"-FIELD:b:VARCHAR",
 	));
